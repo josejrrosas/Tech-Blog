69e5bd149ec4d91a509d2c02445893247fb90e32
Jose Rosas <josejrrosas@yahoo.com>
2023-06-01 12:49:01 -0500

    reverting back to old config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database:process.env.DATABASE,
-  dialect: 'mysql',
-  port: process.env.DB_PORT,
-  host:process.env.DB_HOST,
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
