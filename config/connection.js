978cb571a1fd1b54f210affbede06f6a88f51163
Jose Rosas <josejrrosas@yahoo.com>
2023-06-02 14:41:03 -0500

    added MySQL DB

--- config/connection.js
+++ config/connection.js
@@ -6,21 +6,6 @@ require('dotenv').config();
 
 const urlDB='mysql://root:DxVwu35Wm07pxaFpUmqX@containers-us-west-65.railway.app:5985/railway'
 
-let sequelize;
-
-if (process.env.urlDB) {
-  sequelize = new Sequelize(process.env.urlDB);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const sequelize = sequelize.createConnection(urlDB)
 
 module.exports = sequelize;
