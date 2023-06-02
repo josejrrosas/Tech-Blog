2d58f51ec53964e7c95924f27c6d308190eaee4e
Jose Rosas <josejrrosas@yahoo.com>
2023-06-02 14:34:28 -0500

    added MySQL DB

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const urlDB=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:
-${process.env.MYSQLHOST}:${process.env.MYSQLPORT}:${process.env.MYSQLDATABASE}`
+// const urlDB=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:
+// ${process.env.MYSQLHOST}:${process.env.MYSQLPORT}:${process.env.MYSQLDATABASE}`
+
+const urlDB='mysql://root:DxVwu35Wm07pxaFpUmqX@containers-us-west-65.railway.app:5985/railway'
 
 let sequelize;
 
