9bfdf78d5a1f5587c0260abe19c0f1845b8fb585
Jose Rosas <josejrrosas@yahoo.com>
2023-06-11 21:50:07 -0500

    added refering to coment route in the api index

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,31 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const urlDB=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:
-// ${process.env.MYSQLHOST}:${process.env.MYSQLPORT}:${process.env.MYSQLDATABASE}`
-//const sequelize = sequelize.createConnection(urlDB)
-
 const urlDB='mysql://root:DxVwu35Wm07pxaFpUmqX@containers-us-west-65.railway.app:5985/railway'
 
-// let sequelize = new Sequelize(process.env.urlDB);
-const sequelize = new Sequelize(urlDB, {
-  dialect: 'mysql',
-  operatorsAliases: false,
-})
+// const sequelize = new Sequelize(urlDB, {
+//   dialect: 'mysql',
+//   operatorsAliases: false,
+// })
 
 // let sequelize;
 
-// if (process.env.urlDB) {
-//   sequelize = new Sequelize(process.env.urlDB);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
+if (process.env.urlDB) {
+  sequelize = new Sequelize(process.env.urlDB, {
+  dialect: 'mysql',
+  operatorsAliases: false,
+})
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
